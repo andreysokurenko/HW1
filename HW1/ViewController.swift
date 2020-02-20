daca23f7456ba3f67a7610e9f083b9b753510103
andreysokurenko <andreysokurenko2@gmail.com>
2020-02-21 00:36:21 +0200

    traffic light

--- HW1/ViewController.swift
+++ HW1/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var startButtonLabel: UIButton!
+    
+    var countColor = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
-    }
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
+        
+        redColor.layer.cornerRadius = 75
+        yellowColor.layer.cornerRadius = 75
+        greenColor.layer.cornerRadius = 75
 
+
+        
+        
+    }
+    
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        
+        startButtonLabel.setTitle("Next", for: .normal)
+        
+        switch countColor {
+        case 0:
+            redColor.alpha = 1
+            greenColor.alpha = 0.3
+            countColor = 1
+        case 1:
+            yellowColor.alpha = 1
+            redColor.alpha = 0.3
+            countColor = 2
+        case 2:
+            greenColor.alpha = 1
+            yellowColor.alpha = 0.3
+            countColor = 0
+        default:
+            break
+        }
+        
+    }
+    
+        
+        
+    
+    
 
 }
 
